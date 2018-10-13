18caa3d64814a5ebb45277e85a4aa7dc132f293a
hagaital129 <hagaital129@gmail.com>
2018-10-13 16:50:03 +0300

    calc

---
+++ Calc/1310.js
@@ -0,0 +1,26 @@
+{
+function first(){ document.getElementById("first").innerHTML=formss.text.value +='1';}
+function second(){document.getElementById("second").innerHTML=formss.text.value +='2';}    
+function third(){document.getElementById("third").innerHTML=formss.text.value +='3';}
+function forth(){ document.getElementById("forth").innerHTML=formss.text.value +='4';}
+function fifth(){document.getElementById("fifth").innerHTML=formss.text.value +='5';}    
+function sixth(){document.getElementById("sixth").innerHTML=formss.text.value +='6';}
+function seven(){ document.getElementById("seven").innerHTML=formss.text.value +='7';}
+function eight(){document.getElementById("eight").innerHTML=formss.text.value +='8';}    
+function nine(){document.getElementById("nine").innerHTML=formss.text.value +='9';}
+function plus(){document.getElementById("plus").innerHTML=formss.text.value +='+';}
+function minus(){document.getElementById("minus").innerHTML=formss.text.value +='-';}
+function mana(){document.getElementById("mana").innerHTML=formss.text.value +='/';}
+function kefel(){document.getElementById("kefel").innerHTML=formss.text.value +='*';}
+function zero(){document.getElementById("zero").innerHTML=formss.text.value +='0';}
+function total(){document.getElementById("total").innerHTML=formss.text.value = eval(formss.text.value);}
+
+
+
+
+
+
+
+
+
+}
